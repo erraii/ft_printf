4f8260435e6e07e8d58ebb4a3055090c143b8020
erraii <eraycakiray@gmail.com>
2026-04-30 15:26:03 +0200

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ecakiray <ecakiray@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 17:57:27 by ecakiray          #+#    #+#             */
-/*   Updated: 2026/04/27 16:40:00 by ecakiray         ###   ########.fr       */
+/*   Updated: 2026/04/30 13:45:24 by ecakiray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <stdint.h> 
 //void va_start(va_list ap, last);
 //type va_arg(va_list ap, type);
 //void va_end(va_list ap);
@@ -29,6 +30,24 @@
 // %X hexadecimal uppercase - base 16
 //++ %% percentage sign
 //Your libftprintf.a has to be created at the root of your repository
+
+static void	ft_puthex(unsigned long nb, size_t *size, char c)
+{
+	char	to_print;
+	char	*hex;
+	char	type;
+
+	type = c;
+	if (type == 'x')
+		hex = "0123456789abcdef";
+	else
+		hex = "0123456789ABCDEF";
+	if (nb > 15)
+		ft_puthex (nb / 16, size, type);
+	to_print = *(hex + (nb % 16));
+	write(1, &to_print, 1);
+	(*size)++;
+}
 
 static void	ft_putdigit(long nb, size_t *size)
 {
@@ -47,14 +66,36 @@ static void	ft_putdigit(long nb, size_t *size)
 	(*size)++;
 }
 
-static size_t	ft_print_i_d(int n)
+static size_t	ft_print_i_d_u(long n)
 {
-	long	long_nb;
-	size_t	size;
+	size_t		size;
 
-	long_nb = n;
 	size = 0;
-	ft_putdigit(long_nb, &size);
+	ft_putdigit(n, &size);
+	return (size);
+}
+
+static size_t	ft_print_hex(unsigned long n, char c)
+{
+	size_t		size;
+
+	size = 0;
+	ft_puthex(n, &size, c);
+	return (size);
+}
+
+static size_t	ft_print_p(void *p)
+{
+	size_t		size;
+
+	if (!p)
+	{
+		write(1, "(nil)", 5);
+		return (5);
+	}
+	size = 2;
+	write(1, "0x", 2);
+	ft_puthex((uintptr_t)p, &size, 'x');
 	return (size);
 }
 
@@ -115,13 +156,26 @@ int	ft_printf(const char *string, ...)
 				write(1, toprint, 1);
 				toprint++;
 			}
-			else if (*toprint == 'd' || *toprint == 'i')
+			else if (*toprint == 'd' || *toprint == 'i' || *toprint == 'u')
 			{
-				total_char += ft_print_i_d(va_arg(ap, int));
+				if (*toprint == 'u')
+					total_char += ft_print_i_d_u((long)va_arg(ap, unsigned int));
+				else
+					total_char += ft_print_i_d_u((long)va_arg(ap, int));
+				toprint++;
+			}
+			else if (*toprint == 'x' || *toprint == 'X')
+			{
+				total_char += ft_print_hex(va_arg(ap, unsigned int), *toprint);
+				toprint++;
+			}
+			else if (*toprint == 'p')
+			{
+				total_char += ft_print_p(va_arg(ap, void *));
 				toprint++;
 			}
 		}
-		if (*toprint)
+		else if (*toprint)
 		{
 			total_char++;
 			write(1, toprint, 1);
@@ -133,19 +187,29 @@ int	ft_printf(const char *string, ...)
 // int	main ()
 // {
 // 	int	ret;
-// 	ret = ft_printf("My first string\n");
-// 	printf("%d ",ret);
-// 	ret = ft_printf("%c", '\n');
-// 	printf("%d ",ret);
-// 	ret = ft_printf("%s", "My first string as arg\n");
-// 	printf("%d ",ret);
-// 	ret = ft_printf("Just wanna print percentage sign: %% \n");
-// 	printf("%d ",ret);
-// 	ret = ft_printf(" NULL %s NULL ", NULL);
-// 	printf("%d ",ret);
-// 	ret = ft_printf("%d ", 2147483647);
-// 	printf("%d ",ret);
-// 	ret = ft_printf("%i ", -2147483648);
-// 	printf("%d ",ret);
+// 	// ret = ft_printf("My first string\n");
+// 	// printf("%d ",ret);
+// 	// ret = ft_printf("%c", '\n');
+// 	// printf("%d ",ret);
+// 	// ret = ft_printf("%s", "My first string as arg\n");
+// 	// printf("%d ",ret);
+// 	// ret = ft_printf("Just wanna print percentage sign: %% \n");
+// 	// printf("%d ",ret);
+// 	// ret = ft_printf(" NULL %s NULL ", NULL);
+// 	// printf("%d ",ret);
+// 	// ret = ft_printf("%d ", 2147483647);
+// 	// printf("%d ",ret);
+// 	// ret = ft_printf("%i ", -2147483648);
+// 	// printf("%d ",ret);
+// 	// ret = ft_printf("%u ", 9223372036854775807);
+// 	// printf("%d ",ret);
+// 	// printf("%lu\n", LONG_MAX);
+// 	// printf("%lu\n", LONG_MIN);
+// 	// printf("%lu\n", ULONG_MAX);
+// 	// printf("%lld\n", 9223372036854775807LL);
+// 	ft_printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+// 	//printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+// 	//printf(" %p %p ", 0, 0);
+
 // 	return (0);
 // }
